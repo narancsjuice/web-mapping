692fe4d84ed9a3f42c3309bb402a93b9a787fcc7
Marton Kovacs <narancsjuice@gmail.com>
2022-04-26 15:32:00 +0200

    Add user prompt
    Added a user prompt for getting the starting location of the map based on user
    input.
    
    It checks the user input to be float type and be a valid lat/lon coordinate. It also
    has a default value should the user wish to not use their own coordinates.
    
    GH-3

--- web-map.py
+++ web-map.py
@@ -1,7 +1,54 @@
 import folium
 
-# TODO: ask for user input on where the starting location of the map should be
-# Budapest coordinates
-map = folium.Map(location=[47.497913, 19.040236], zoom_start=7, tiles='Stamen Terrain')
+# TODO: rework user input for location into functions
+location = []
+print("Enter the location coordinates for your map's starting point. If you "
+      "want to skip, leave the prompt empty and you'll be defaulted to Budapest"
+      " as the starting point.")
+
+lat_text = "Enter Latitude of start location (0° to 90°): "
+lon_text = "Enter Longitude  of start location (-180° to 180°): "
+invalid_text = "Invalid coordinate."
+lat = input(lat_text)
+lon = input(lon_text)
+
+location = [lat, lon]
+
+invalid_coordinate = [True, True]
+
+for index, coordinate in enumerate(location):
+    while coordinate != "" and invalid_coordinate[index] is True:
+        try:
+            coordinate = float(coordinate)
+            location[index] = coordinate
+            if index == 0:
+                if 90 >= location[0] >= 0:
+                    invalid_coordinate[index] = False
+                else:
+                    coordinate = input(invalid_text + lat_text)
+            elif index == 1:
+                if 180 >= location[1] >= -180:
+                    invalid_coordinate[index] = False
+                else:
+                    coordinate = input(invalid_text + " " + lon_text)
+        except:
+            if index == 0:
+                value = lat_text
+            elif index == 1:
+                value = lon_text
+            coordinate = input(
+                f"Text is not accepted. {value}")
+            continue
+    else:
+        if index == 0 and coordinate == "":
+            lat = 47.497913
+            location[index] = lat
+            print(f"Latitude is set by default to {lat}.")
+        elif index == 1 and coordinate == "":
+            lon = 19.040236
+            location[index] = lon
+            print(f"Latitude is set by default to {lon}.")
+
+map = folium.Map(location=[location[0], location[1]], zoom_start=7, tiles="Stamen Terrain")
 
 map.save("map1.html")
